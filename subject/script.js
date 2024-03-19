9cb6da3ef4add833d4ba0e51b91f31f2bad2a2ae
Marion Chatard <marion.chatard@etu.minesparis.psl.eu>
2024-03-19 20:24:54 +0100

    début de la fenêtre d'affichage et définition de la langue etc

--- subject/script.js
+++ subject/script.js
@@ -7,4 +7,12 @@
 // This is the starter file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
 
 // # START EDITING YOUR JAVASCRIPT HERE
+
 // ===============
+
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Calculatrice</title>
+</head>
